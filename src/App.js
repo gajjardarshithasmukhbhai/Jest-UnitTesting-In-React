143d608538a476367ec14ea30a6fb1bdbee32ab9
gajjardarshithasmukhbhai <darshit.hasmukhbhai1998@gmail.com>
2022-07-19 02:15:35 +0530

    feature: file is changed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        style={{
+          width: "50%",
+          display: "flex",
+          justifyContent: "center",
+          border: "1px solid black",
+          margin: "10%",
+          marginLeft: "25%",
+          marginTop: "20%",
+        }}
+      >
+        <h1>Hello Darshit Welcome in Jest Series</h1>
+      </div>
+    </>
   );
 }
 
